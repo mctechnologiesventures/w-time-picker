c843aa49ce33694931cb56c63ee5f650ec7f0079
rvcarlig <raducarlig@gmail.com>
2023-01-07 12:43:19 +0200

    fix(package): export classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import TimePicker from './components/TimePicker';
+import TimePickerSelection from './components/TimePickerSelection';
+import HourWheel from './components/HourWheel';
+import MinuteWheel from './components/MinuteWheel';
+import HourFormat from './components/HourFormat';
 
-export { TimePicker };
+export { TimePicker, TimePickerSelection, HourFormat, HourWheel, MinuteWheel };
